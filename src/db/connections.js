19fb04965cb047534233e3c5a2f750f4c06e16b4
Suzair0318 <128297059+Suzair0318@users.noreply.github.com>
2023-12-03 20:31:44 +0500

    Update connections.js

--- src/db/connections.js
+++ src/db/connections.js
@@ -1,7 +1,7 @@
 const  mongoose = require('mongoose');
 
 
- const DatabaseConnection  = mongoose.connect( ``mongodb+srv://Nazcollection_api:Nazcollection_api@cluster0.hpy8te0.mongodb.net/`` ).then(() => {
+ const DatabaseConnection  = mongoose.connect(`mongodb+srv://Nazcollection_api:Nazcollection_api@cluster0.hpy8te0.mongodb.net/`).then(() => {
    console.log("Mongo_DB Connection");
 }).catch((e) =>{
     console.log(e);
